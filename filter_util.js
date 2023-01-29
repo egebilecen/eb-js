b6b276a97b4ad41f1215c463f4d006eaa13942b0
Ege Bilecen <29331682+egebilecen@users.noreply.github.com>
2023-01-29 15:21:22 +0300

    Update filter_util.js

--- filter_util.js
+++ filter_util.js
@@ -1,3 +1,15 @@
+function isNumber(n) 
+{
+    return !isNaN(parseFloat(n)) && !isNaN(n - 0);
+}
+
+function validateEmail(email)
+{
+    return email.match(
+        /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
+}
+
 /*
 	Add number filter to input. Requires jQuery.
 */
